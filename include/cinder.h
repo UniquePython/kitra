5558f0ea8789b97ce4f9b3ec103de95284034c17
Ananyo Bhattacharya <ananyobhattacharya@gmail.com>
2026-05-02 10:33:05 +0530

    Add SDL-based window creation and destruction

--- include/cinder.h
+++ include/cinder.h
@@ -3,6 +3,7 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <limits.h>
 
 typedef struct s_cinder_ctx CinderCtx;
 
@@ -10,23 +11,60 @@ typedef enum
 {
     CINDER_STATUS_OK,
     CINDER_STATUS_SUBSYSTEM_INIT_FAILED,
+    CINDER_STATUS_WINDOW_CREATE_FAILED,
+    CINDER_STATUS_RENDERER_CREATE_FAILED,
+
 } CinderStatus;
+
+// ======================================= SUBSYSTEM ================================================
 
 typedef enum
 {
     CINDER_SUBSYSTEM_NONE = 0,
 
-    CINDER_SUBSYSTEM_EVENTS = 1u << 0, // 0x01
-    CINDER_SUBSYSTEM_AUDIO = 1u << 1,  // 0x02
-    CINDER_SUBSYSTEM_VIDEO = 1u << 2,  // 0x04
+    CINDER_SUBSYSTEM_EVENTS = 1u << 0,
+    CINDER_SUBSYSTEM_AUDIO = 1u << 1,
+    CINDER_SUBSYSTEM_VIDEO = 1u << 2,
     CINDER_SUBSYSTEM_ALL = CINDER_SUBSYSTEM_EVENTS | CINDER_SUBSYSTEM_AUDIO | CINDER_SUBSYSTEM_VIDEO
+
 } CinderSubsystem;
 
 CinderStatus CinderInit(CinderSubsystem flags);
 void CinderQuit(void);
 
-CinderStatus CinderCreateWindow(const char *title, size_t width, size_t height);
-CinderStatus CinderDestroyWindow(void);
+// ======================================= WINDOW ================================================
+
+typedef enum
+{
+    CINDER_WINDOW_NONE = 0,
+
+    CINDER_WINDOW_SHOWN = 1u << 0,
+    CINDER_WINDOW_RESIZABLE = 1u << 1,
+    CINDER_WINDOW_FULLSCREEN = 1u << 2,
+    CINDER_WINDOW_BORDERLESS = 1u << 3,
+    CINDER_WINDOW_HIDDEN = 1u << 4,
+
+} CinderWindowFlags;
+
+typedef struct CinderWindowDesc
+{
+    const char *title;
+    size_t width;
+    size_t height;
+
+    int x;
+    int y;
+
+    CinderWindowFlags flags;
+
+} CinderWindowDesc;
+
+#define CINDER_WINDOW_POS_CENTERED INT_MIN
+
+CinderStatus CinderCreateWindow(const CinderWindowDesc *winDesc);
+void CinderDestroyWindow(void);
+
+// ======================================= ERROR ================================================
 
 const char *CinderGetError(void);
 
--- src/cinder.c
+++ src/cinder.c
@@ -10,10 +10,13 @@ typedef struct CinderCtx
     SDL_Renderer *renderer;
 
     bool isRunning;
+
 } CinderCtx;
 
 CinderCtx gCinderCtx;
 const char *gCinderErrMsg = "";
+
+// ======================================= SUBSYSTEM ================================================
 
 static void CinderRollbackSubsystems(uint32_t flags)
 {
@@ -79,18 +82,104 @@ CinderStatus CinderInit(CinderSubsystem flags)
 
 void CinderQuit(void)
 {
+    CinderDestroyWindow();
+
     gCinderCtx.initFlags = 0;
 
     SDL_Quit();
 }
 
-CinderStatus CinderCreateWindow(const char *title, size_t width, size_t height)
+// ======================================= WINDOW ================================================
+
+static uint32_t CinderToSDLWindowFlags(CinderWindowFlags flags)
 {
+    uint32_t sdlFlags = 0;
+
+    if (flags & CINDER_WINDOW_SHOWN)
+        sdlFlags |= SDL_WINDOW_SHOWN;
+
+    if (flags & CINDER_WINDOW_RESIZABLE)
+        sdlFlags |= SDL_WINDOW_RESIZABLE;
+
+    if (flags & CINDER_WINDOW_FULLSCREEN)
+        sdlFlags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
+
+    if (flags & CINDER_WINDOW_BORDERLESS)
+        sdlFlags |= SDL_WINDOW_BORDERLESS;
+
+    if (flags & CINDER_WINDOW_HIDDEN)
+        sdlFlags |= SDL_WINDOW_HIDDEN;
+
+    return sdlFlags;
 }
 
-CinderStatus CinderDestroyWindow(void)
+CinderStatus CinderCreateWindow(const CinderWindowDesc *winDesc)
 {
+    CinderWindowDesc internalWinDesc = {0};
+
+    if (!winDesc)
+    {
+        internalWinDesc.title = "Cinder";
+        internalWinDesc.width = 900;
+        internalWinDesc.height = 600;
+        internalWinDesc.x = SDL_WINDOWPOS_CENTERED;
+        internalWinDesc.y = SDL_WINDOWPOS_CENTERED;
+        internalWinDesc.flags = CINDER_WINDOW_SHOWN | CINDER_WINDOW_RESIZABLE;
+    }
+    else
+    {
+        internalWinDesc.title = winDesc->title;
+        internalWinDesc.width = winDesc->width;
+        internalWinDesc.height = winDesc->height;
+        internalWinDesc.x = winDesc->x;
+        internalWinDesc.y = winDesc->y;
+        internalWinDesc.flags = winDesc->flags;
+    }
+
+    gCinderCtx.window = SDL_CreateWindow(
+        internalWinDesc.title,
+        internalWinDesc.x == CINDER_WINDOW_POS_CENTERED ? SDL_WINDOWPOS_CENTERED : internalWinDesc.x,
+        internalWinDesc.y == CINDER_WINDOW_POS_CENTERED ? SDL_WINDOWPOS_CENTERED : internalWinDesc.y,
+        (int)internalWinDesc.width, (int)internalWinDesc.height,
+        CinderToSDLWindowFlags(internalWinDesc.flags));
+
+    if (!gCinderCtx.window)
+    {
+        gCinderErrMsg = SDL_GetError();
+        return CINDER_STATUS_WINDOW_CREATE_FAILED;
+    }
+
+    gCinderCtx.renderer = SDL_CreateRenderer(gCinderCtx.window, -1, SDL_RENDERER_ACCELERATED);
+
+    if (!gCinderCtx.renderer)
+    {
+        gCinderErrMsg = SDL_GetError();
+
+        SDL_DestroyWindow(gCinderCtx.window);
+        gCinderCtx.window = NULL;
+
+        return CINDER_STATUS_RENDERER_CREATE_FAILED;
+    }
+
+    return CINDER_STATUS_OK;
 }
+
+void CinderDestroyWindow(void)
+{
+    if (gCinderCtx.renderer)
+    {
+        SDL_DestroyRenderer(gCinderCtx.renderer);
+        gCinderCtx.renderer = NULL;
+    }
+
+    if (gCinderCtx.window)
+    {
+        SDL_DestroyWindow(gCinderCtx.window);
+        gCinderCtx.window = NULL;
+    }
+}
+
+// ======================================= ERROR ================================================
 
 const char *CinderGetError(void)
 {
